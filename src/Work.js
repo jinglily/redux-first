604df6ef4a05e81b13961e9ee4d224e387a2f6fb
jingli <351201853@qq.com>
2016-12-27 10:58:42 +0800

    i

--- src/Work.js
+++ src/Work.js
@@ -1,10 +1,33 @@
 import React from 'react';
 
 class Work extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      title:['张张 ：aaaa']
+
+    }
+  }
+  submitClick(e){
+    e.preventDefault();
+    let title = this.refs.title.value;
+    let content = this.refs.content.value;
+    let data = [title + ' ： ' + content];
+    this.setState({
+      title:this.state.title.concat(data),
+    });
+    console.log(this.state.title);
+  }
    render(){
+     let text = this.state.title.map((item,i) => <div className='comment' key={i}>{item}</div>)
      return(
        <div className="work">
-          Work
+         <div className="content">{text}</div>
+          <form onSubmit={this.submitClick.bind(this)}>
+            <input className="put" ref="title"/><br />
+            <input className="put" ref="content"/><br />
+            <button className="btn">评论</button>
+          </form>
        </div>
      )
    }
